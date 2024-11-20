e4a6a9b368b04e92af801b6cf290e5098aebb45a
Son,Haegwang <sonhaiguang21@gmail.com>
2024-11-20 09:26:21 +0900

    two

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
       System.out.println("201914112 손해광");
+      System.out.println(Hello.sum(10));
+      System.out.println(Hello.sum(100));
     }
   }
   
